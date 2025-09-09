78b202f80706155b693dfcd864e70dc8cf09151a
JDG <ktkt1413@naver.com>
2025-09-09 21:29:59 +0900

    inital commit

---
+++ src/main/java/com/dayaeyak/backofficservice/BackofficServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dayaeyak.backofficservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dayaeyak/backofficservice/BackofficServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dayaeyak.backofficservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackofficServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
